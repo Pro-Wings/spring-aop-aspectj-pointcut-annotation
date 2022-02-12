765bbda4b27bfd23ad3fd9f323e8432b29cac4b4
JD <jayprowings@gmail.com>
2022-02-12 11:57:27 +0530

    first commit

---
+++ src/main/java/org/websparrow/aspect/LogBeforeAdvice.java
@@ -0,0 +1,25 @@
+package org.websparrow.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogBeforeAdvice {
+
+	// It will execute only for Bank's deposit method
+	@Pointcut("execution(* org.websparrow.service.Bank.*(..))")
+	public void depositPointcut() {
+	}
+
+	@Before("depositPointcut()")
+	public void logBefore() {
+
+		System.out.println(".............I WILL EXECUTE ONLY BEFORE DEPOSIT METHOD.............");
+		System.out.println(".............>>>>>BEFORE DEPOSIT METHOD<<<<.............");
+
+	}
+	
+}
